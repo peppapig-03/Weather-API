32f17d2ec0e0af22f09172ce529cbaee84674fd1
peppapig-03 <245528390+peppapig-03@users.noreply.github.com>
2026-05-20 20:28:52 +0800

    fix deployment

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,6 +2,8 @@ import locationPage from "./pages/location/locationPage.js"
 import emailPage from "./pages/email/emailPage.js"
 locationPage.spawn()
 locationPage.despawn()
+locationPage.spawn()
+locationPage.despawn()
 emailPage.spawn()
 
 
