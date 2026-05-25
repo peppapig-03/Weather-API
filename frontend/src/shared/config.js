711fde36a9e036315ad1401a2af2b71d603c29a7
peppapig-03 <245528390+peppapig-03@users.noreply.github.com>
2026-05-25 22:14:46 +0800

    Deployment commit

--- frontend/src/shared/config.js
+++ frontend/src/shared/config.js
@@ -1,4 +1,4 @@
 const config={
-    API_URL:window.location.hostname=="localhost"?"http://localhost:4000":"https://weather-api-kqnp.onrender.com"
+    API_URL:window.location.hostname=="localhost"?"http://localhost:4000":"https://weather-api-j6ya.onrender.com"
 }
 export default config
