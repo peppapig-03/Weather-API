390413f3c6a34a3d74a2178f8771b3a6a5cfcfd1
peppapig-03 <245528390+peppapig-03@users.noreply.github.com>
2026-05-26 14:33:54 +0800

    edited yaml

--- app.js
+++ app.js
@@ -17,7 +17,3 @@ app.use("/emails/", emailRouter)
 app.use("/locations/", locationRouter)
 app.use("/subscriptions/", subscriptionRouter)
 app.use("/sendemail/", sendEmailRouter)
-app.get("/",(req,res)=>{
-    const now=new Date()
-    res.json(now.toLocaleTimeString('en-GB').slice(0,2))
-})
