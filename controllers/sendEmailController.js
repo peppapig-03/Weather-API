ffe22cc339932794587e329db784ccd1b1327f54
peppapig-03 <245528390+peppapig-03@users.noreply.github.com>
2026-05-26 09:25:17 +0800

    added automation

--- controllers/sendEmailController.js
+++ controllers/sendEmailController.js
@@ -1,12 +1,27 @@
 import sendEmailFlow from "../serviceFlows/sendEmailFlow.js"
 import utils from "../shared/utils.js"
-const sendEmailController=async function(req,res){
-    const emailAddress=req.params.email
-    try{
-        const array=await sendEmailFlow(emailAddress)
-        res.json(utils.successfulJSON("GET_WEATHER_SUCCESS", array))
-    } catch(error){
-        res.status(error.status||500).json(utils.errorJSON(error.status||500, error.message))
+import allEmailFlow from "../serviceFlows/allEmailFlow.js"
+const sendEmailController=(function(){
+    const sendOneEmail=async function(req,res){
+        const emailAddress=req.params.email
+        try{
+            const array=await sendEmailFlow(emailAddress)
+            res.json(utils.successfulJSON("SEND_EMAIL_SUCCESS", array))
+        } catch(error){
+            res.status(error.status||500).json(utils.errorJSON(error.status||500, error.message))
+        }
     }
-}
+    const sendAllEmails=async function(req,res){
+        try{
+            await allEmailFlow()
+            res.json(utils.successfulJSON("SEND_ALL_EMAILS_SUCCESS"))
+        } catch(error){
+            res.status(error.status||500).json(utils.errorJSON(error.status||500, error.message))
+        }
+    }
+    return {
+        sendOneEmail,
+        sendAllEmails
+    }
+})()
 export default sendEmailController
--- routers/sendEmailRouter.js
+++ routers/sendEmailRouter.js
@@ -1,7 +1,6 @@
 import express from "express"
 import sendEmailController from "../controllers/sendEmailController.js"
 const sendEmailRouter=express.Router()
-sendEmailRouter.get("/:email", async (req,res)=>{
-    sendEmailController(req,res)
-})
+sendEmailRouter.get("/automate", sendEmailController.sendAllEmails)
+sendEmailRouter.get("/:email", sendEmailController.sendOneEmail)
 export default sendEmailRouter
