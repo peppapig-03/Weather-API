fed1ac15f711842c4d481dab10baf0f84002cc2e
peppapig-03 <245528390+peppapig-03@users.noreply.github.com>
2026-05-26 16:33:10 +0800

    change config

--- frontend/src/shared/config.js
+++ frontend/src/shared/config.js
@@ -1,4 +1,4 @@
 const config={
-    API_URL:window.location.hostname=="localhost"?"http://localhost:4000":"https://weather-api-j6ya.onrender.com"
+    API_URL:window.location.hostname=="localhost"?"http://localhost:4000":"https://weather-api-4pxd.onrender.com"
 }
 export default config
