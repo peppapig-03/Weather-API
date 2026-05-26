a1c6ff8d572215086d30813c16c007e5880858df
peppapig-03 <245528390+peppapig-03@users.noreply.github.com>
2026-05-26 09:42:08 +0800

    Server commit

--- automation/auto.js
+++ automation/auto.js
@@ -1,9 +1,7 @@
 const emailAutomation=async function(){
     const now = new Date()
     const timeString = now.toLocaleTimeString('en-GB')
-    console.log(timeString)
     if (["07","08","09","11","12","15","16"].includes(timeString.slice(0,2))){
-        console.log("https://weather-api-j6ya.onrender.com/sendemail/automate")
         const response=await fetch("https://weather-api-j6ya.onrender.com/sendemail/automate")
     }
     
