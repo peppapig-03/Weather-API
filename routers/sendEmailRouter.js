d137de7b2c6a21024531c8f9df86bbab71dc1abe
peppapig-03 <245528390+peppapig-03@users.noreply.github.com>
2026-05-26 16:30:24 +0800

    gave backend timecheck

--- routers/sendEmailRouter.js
+++ routers/sendEmailRouter.js
@@ -2,5 +2,6 @@ import express from "express"
 import sendEmailController from "../controllers/sendEmailController.js"
 const sendEmailRouter=express.Router()
 sendEmailRouter.get("/automate", sendEmailController.sendAllEmails)
+sendEmailRouter.get("/test", sendEmailController.testAllEmails)
 sendEmailRouter.get("/:email", sendEmailController.sendOneEmail)
 export default sendEmailRouter
---
+++ services/keyService.js
@@ -0,0 +1,25 @@
+import dotenv from "dotenv"
+dotenv.config()
+const keyService=(function(){
+    const validateKey=function(attemptedKey){
+        if (attemptedKey!=process.env.CRON_JOB_KEY){
+            const newError=new Error("Wrong API Key")
+            newError.status=403
+            throw newError
+        }
+    }
+    const validateTime=function(){
+        const now=new Date()
+        const timeString=now.toLocaleTimeString('en-GB')
+        if (["07","08","11","12","15","16"].includes(timeString.slice(0,2))){
+            return true
+        } else{
+            return false
+        }
+    }
+    return {
+        validateKey,
+        validateTime
+    }
+})()
+export default keyService
